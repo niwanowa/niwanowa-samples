ebfb721190893cf0258b173bcc490efebef580bd
niwanowa <40478818+niwanowa@users.noreply.github.com>
2024-02-24 17:26:56 +0900

    sampleコードのfnuction化とアーティスト名の取得処理追加

--- youtube-data-api/src/scraping_livefans.py
+++ youtube-data-api/src/scraping_livefans.py
@@ -2,19 +2,34 @@ import requests
 from bs4 import BeautifulSoup
 
 # URLを指定
-url = "https://www.livefans.jp/events/1643325"
+URL = "https://www.livefans.jp/events/1643325"
 
-# ページの内容を取得
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
 
-# 'div'タグで'class'が'setBlock nopscr'の要素を抽出
-parent_div = soup.find("div", class_="setBlock nopscr")
+def scraping_livefans(url):
+    setlist = []
+    # ページの内容を取得
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
 
-# 親要素が見つかった場合、その子要素の中からクラスが'ttl'の要素を取得
-if parent_div:
-    ttl_elements = parent_div.find_all(class_="ttl")
-    for i, element in enumerate(ttl_elements, start=1):
-        print(f"{i}. {element.text.strip()}")
-else:
-    print("'setBlock nopscr'クラスを持つdiv要素が見つかりませんでした。")
+    artistName = soup.find("h4", class_="artistName2").text.strip()
+    print(artistName)
+
+    # 'div'タグで'class'が'setBlock nopscr'の要素を抽出
+    parent_div = soup.find("div", class_="setBlock nopscr")
+
+    # 親要素が見つかった場合、その子要素の中からクラスが'ttl'の要素を取得
+    if parent_div:
+        ttl_elements = parent_div.find_all(class_="ttl")
+        for i, element in enumerate(ttl_elements, start=1):
+            # elementの中からaタグのテキストを取得
+            a_tag = element.find("a").text
+            setlist.append(a_tag)
+    else:
+        raise Exception("指定した要素が見つかりませんでした。")
+
+    return artistName, setlist
+
+
+if __name__ == "__main__":
+    setlist = scraping_livefans(URL)
+    print(setlist)
