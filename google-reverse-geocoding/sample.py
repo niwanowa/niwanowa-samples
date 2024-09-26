666139d220b5016bbce676d6cab9a42a98554a64
niwanowa <40478818+niwanowa@users.noreply.github.com>
2024-09-26 16:03:02 +0900

    connpass APIのレスポンスでも実行できるかの確認

--- google-reverse-geocoding/sample.py
+++ google-reverse-geocoding/sample.py
@@ -11,6 +11,6 @@ gmaps = googlemaps.Client(key=os.getenv("GOOGLE_MAPS_API_KEY"))
 
 
 # Look up an address with reverse geocoding
-reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+reverse_geocode_result = gmaps.reverse_geocode((33.593111000000, 130.401897800000))
 
 print(json.dumps(reverse_geocode_result, indent=4, ensure_ascii=False))
